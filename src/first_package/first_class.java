af918366978a2cd186dbc6433624c4bc24b0c3ed
ibrahim <saffetk502@gmail.com>
2021-07-25 15:39:35 +0200

    sorun tekrar ele alindi

--- src/first_package/first_class.java
+++ src/first_package/first_class.java
@@ -6,11 +6,11 @@ public class first_class {
 		
 		System.out.println("first commit from local repository");
 		System.out.println("second commit from local repository");
-<<<<<<< HEAD
+
 		System.out.println("ucuncu commit from local repository");
-=======
+
 		System.out.println("Conflict yapmak istiyorum");
->>>>>>> refs/remotes/origin/master
+
 		System.out.println("first commit from remote repository");
 	}
 
